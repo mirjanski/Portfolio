4ce2cf56661d78a23c8011a2bdb28ba682ae312d
mirjanski <dzhumashov.mirzhan@gmail.com>
2025-10-15 12:04:47 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -38,9 +38,19 @@ document.addEventListener('keydown', (e) => {
 
 
 
+const backToTopBtn = document.getElementById("backToTop");
 
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+};
 
-
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
 
 
